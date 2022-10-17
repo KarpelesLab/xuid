6eb3c9672e0bc2e6e0b279fe975a0d52c85f1f97
Mark Karpeles <magicaltux@gmail.com>
2022-10-17 21:17:09 +0900

    add scan rawbytes

--- scan.go
+++ scan.go
@@ -1,12 +1,23 @@
 package xuid
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+)
 
 // Scan sets the value of the XUID to the passed data, parsing it as needed.
 func (x *XUID) Scan(value any) error {
 	switch v := value.(type) {
 	case string:
 		nv, err := Parse(v)
+		if err != nil {
+			return err
+		}
+		x.Prefix = nv.Prefix
+		x.UUID = nv.UUID
+		return nil
+	case sql.RawBytes:
+		nv, err := Parse(string(v))
 		if err != nil {
 			return err
 		}
